eef385e79d3a0a2f4faf8eefe1bff8ae24b6960c
kailashrao <kailash7rao@gmail.com>
2023-12-17 11:41:12 -0800

    Organize + Generate Calendar for any year

--- calendar.js
+++ calendar.js
@@ -1,73 +1,86 @@
  let year1 = 2024
- 
- let months1 = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
- let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+ generateCalendar(year1)
 
- year1%4 == 0 ? daysInMonth[1] = 29 : daysInMonth[1] = 28;
- 
- for(let k = 0; k < months1.length; k++) {
+ function generateCalendar(year) {
+    let allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    let table1 = "";
-    table1 += "<caption>" + months1[k] + " " + year1 + "</caption>";
+    isLeapYear(year1) ? daysInMonth[1] = 29 : daysInMonth[1] = 28;
+    
+    for(let k = 0; k < allMonths.length; k++) {
 
-    let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    for(let day in daysOfWeek) {
-        table1 += "<th>" + daysOfWeek[day] + "</th>";
-    }
+        let monthTable = "<caption>" + allMonths[k] + " " + year1 + "</caption>";
 
-    let rows1 = 6;
-    let columns1 = 7;
-
-    let startDate = getFirstSunday(year1, months1[k]).getDate();
-    let isPrevMonth = false;
-    let displayNum = startDate;
-    let endOfMonth = false;
-
-    for(let j = 0; j < rows1; j++) {
-        table1 += "<tr>";
-        let prevMonth;
-        k == 0 ? prevMonth = 11 : prevMonth = k-1; 
-
-        for(let i = 0; i < columns1; i++) {
-            if(j == 0 && displayNum > 7)
-                isPrevMonth = true;
-            else
-                isPrevMonth = false;
-
-            if( j == 0 && displayNum > daysInMonth[prevMonth]) {
-                displayNum -= daysInMonth[prevMonth];
-                isPrevMonth = false;
-            }
-
-            if(j >= 4  && displayNum > daysInMonth[k]) {
-                displayNum -= daysInMonth[k];
-                endOfMonth = true;
-                break; 
-            }
-
-            if(isPrevMonth)
-                table1 += "<td>" + " " + "</td>";
-            else
-                table1 += "<td>" + displayNum + "</td>";
-
-            displayNum++;
+        let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        for(let i = 0; i < daysOfWeek.length; i++) {
+            monthTable += "<th>" + daysOfWeek[i] + "</th>";
         }
 
-        table1 += "</tr>";
+        let rows = 6;
+        let columns = 7;
 
-        if(endOfMonth)
-            break;
+        let startDate = getFirstSunday(year1, allMonths[k]).getDate();
+        let displayNum = startDate;
+        let isPrevMonth = false;
+        let endOfMonth = false;
+
+        for(let j = 0; j < rows; j++) {
+            monthTable += "<tr>";
+
+            let prevMonth;
+            k == 0 ? prevMonth = 11 : prevMonth = k-1; 
+
+            for(let i = 0; i < columns; i++) {
+                if(j == 0 && displayNum > 7)
+                    isPrevMonth = true;
+                else
+                    isPrevMonth = false;
+
+                if( j == 0 && displayNum > daysInMonth[prevMonth]) {
+                    displayNum -= daysInMonth[prevMonth];
+                    isPrevMonth = false;
+                }
+
+                if(j >= 4  && displayNum > daysInMonth[k]) {
+                    displayNum -= daysInMonth[k];
+                    endOfMonth = true;
+                    break; 
+                }
+
+                if(isPrevMonth)
+                    monthTable += "<td>" + " " + "</td>";
+                else
+                    monthTable += "<td>" + displayNum + "</td>";
+
+                displayNum++;
+            }
+
+            monthTable += "</tr>";
+
+            if(endOfMonth)
+                break;
+        }
+
+        document.getElementById(allMonths[k]).innerHTML = monthTable;
     }
+}
 
-    document.getElementById(months1[k]).innerHTML = table1;
- }
+function isLeapYear(year) {
+    let leap = false;
+    if (year % 4 == 0 && year % 100 != 0)
+        leap = true;
+    else if (year % 100 == 0 && year % 400 == 0)
+        leap = true;
+
+    return leap;
+}
 
  function getFirstSunday(year, month) {
-    let date1 = new Date("01-" + month + "-" + year);
-    let day1 = date1.getDay();
+    let firstOfMonth = new Date("01-" + month + "-" + year);
+    let sundayOffset = firstOfMonth.getDay();
 
-    let ms = date1.getTime() - (day1*86400000);
-    let date2 = new Date(ms);
-    return date2;
+    let ms = firstOfMonth.getTime() - (sundayOffset*86400000);
+    let firstSunday = new Date(ms);
+    return firstSunday;
     
 }
